8a76b61fa1632e2a6f04ef04dee4cc5037c0aa38
bar_haimi <barhaimi2512@gmail.com>
2025-01-24 14:12:42 +0200

    new db changed to include song table and search function

---
+++ backend/src/db/migrations/0000_talented_jack_murdock.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `playlistTable` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`user_id` integer NOT NULL,
+	`name` text(30) NOT NULL,
+	`is_private` integer DEFAULT 0 NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `songHistory` (
+	`user_id` integer NOT NULL,
+	`song_id` integer NOT NULL,
+	`last_listen` text NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`song_id`) REFERENCES `songsTable`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `songsInPlaylistTable` (
+	`playlist_id` integer NOT NULL,
+	`song_id` integer NOT NULL,
+	FOREIGN KEY (`playlist_id`) REFERENCES `playlistTable`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`song_id`) REFERENCES `songsTable`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `songsTable` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`duration` integer,
+	`file_path` text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`username` text(20) NOT NULL,
+	`password` text NOT NULL
+);
--- backend/src/db/migrations/0002_red_randall.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `playlistTable` ADD `is_private` integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE `songHistory` DROP COLUMN `is_private`;
